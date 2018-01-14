0a445e4eda768867cef48ce5f0957f87d4d2991d
Sean Smith <sean.smithy1@gmail.com>
2018-01-14 20:15:34 +1000

    Moved fontello to app/assets - modified manifest to include fonts/

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,20 +6,12 @@ Rails.application.config.assets.precompile += %w( payment_details.png )
 
 Rails.application.config.assets.precompile
 
-#Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
-#Rails.application.config.assets.precompile += %w( fontello.css )
-
 # Precompile Vendor JS Assets 
 Rails.application.config.assets.precompile += %w( plugins/rs-plugin-5/index.js )
 Rails.application.config.assets.precompile += %w( plugins/modernizr.js )
 Rails.application.config.assets.precompile += %w( theproject/template.js )
 
-# PreCompile Vendor CSS Assets
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
-Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
-
-Rails.application.config.assets.precompile += %w( fontello/css/fontello.css )
 Rails.application.config.assets.precompile += %w( plugins/rs-plugin-5/css/layers.css )
 Rails.application.config.assets.precompile += %w( plugins/rs-plugin-5/css/navigation.css )
 Rails.application.config.assets.precompile += %w( plugins/rs-plugin-5/css/settings.css )
