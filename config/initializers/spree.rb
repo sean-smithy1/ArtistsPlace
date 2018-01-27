2be1f40130ccc2ffcf161512c2f41c5c607d5e18
Sean Smith <sean.smithy1@gmail.com>
2018-01-28 09:51:56 +1000

    Modify Image Sizes

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -17,40 +17,39 @@ end
 
 Spree.user_class = "Spree::LegacyUser"
 
+  attachment_config = {
 
-attachment_config = {
+    s3_credentials: {
 
-  s3_credentials: {
+      access_key_id:     Rails.application.secrets[:aws_access_key],
+  #    access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: Rails.application.secrets[:aws_secret_access_key],
+  #    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      bucket:            Rails.application.secrets[:aws_bucket]
+  #    bucket:            ENV['AWS_BUCKET']
+    
+    },
 
-    access_key_id:     Rails.application.secrets[:aws_access_key],
-#    access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-    secret_access_key: Rails.application.secrets[:aws_secret_access_key],
-#    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    bucket:            Rails.application.secrets[:aws_bucket]
-#    bucket:            ENV['AWS_BUCKET']
-  
-  },
+    storage:        :s3,
+    s3_region:      "ap-southeast-2",
+    s3_headers:     { "Cache-Control" => "max-age=31557600" },
+    s3_protocol:    "https",
+    bucket:         Rails.application.secrets[:aws_bucket],
+    url:            ":s3_domain_url",
 
-  storage:        :s3,
-  s3_region:      "ap-southeast-2",
-  s3_headers:     { "Cache-Control" => "max-age=31557600" },
-  s3_protocol:    "https",
-  bucket:         Rails.application.secrets[:aws_bucket],
-  url:            ":s3_domain_url",
+    :styles => {
+        :mini =>    "48x48>",
+        :small =>   "375",
+        :product =>  "375",
+        :large =>    "600x600>"
+    },
 
-  styles: {
-      mini:     "48x48>",
-      small:    "100x100>",
-      product:  "375x375>",
-      large:    "600x600>"
-  },
+    path:           "/:class/:id/:style/:basename.:extension",
+    default_url:    "/:class/:id/:style/:basename.:extension",
+    default_style:  "product"
+  }
 
-  path:           "/:class/:id/:style/:basename.:extension",
-  default_url:    "/:class/:id/:style/:basename.:extension",
-  default_style:  "product"
-}
-
-attachment_config.each do |key, value|
-  Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
-end
+  attachment_config.each do |key, value|
+    Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
+  end
 
